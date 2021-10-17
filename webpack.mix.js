767b17f028e9cf74a647849e68dc45f90c321ce4
korcht <89439292+korcht@users.noreply.github.com>
2021-10-17 18:53:23 -0300

    Add files via upload

---
+++ webpack.mix.js
@@ -0,0 +1,35 @@
+const mix = require('laravel-mix');
+
+/*
+|--------------------------------------------------------------------------
+| Mix Asset Management
+|--------------------------------------------------------------------------
+|
+| Mix provides a clean, fluent API for defining some Webpack build steps
+| for your Laravel applications. By default, we are compiling the CSS
+| file for the application as well as bundling up all the JS files.
+|
+*/
+
+mix.js('resources/assets/js/app.js', 'public/js')
+.js('resources/assets/js/backend.js', 'public/js')
+.js('resources/assets/js/manager.js', 'public/js')
+.sass('resources/assets/sass/app.scss', 'public/css')
+.sass('resources/assets/sass/backend.scss', 'public/css')
+.sass('resources/assets/sass/manager.scss', 'public/css')
+.copy('resources/assets/sass/helpers', 'public/css', true)
+.copy('resources/assets/themes', 'public/themes', true)
+.copy('resources/assets/logos', 'public/logos', true)
+.copy('resources/assets/js/helpers', 'public/js/helpers', true)
+.copy('resources/assets/img', 'public/img', true)
+.copy('resources/assets/ayuda', 'public/ayuda', true)
+.copy('resources/assets/uploads', 'public/uploads', true)
+.copy('resources/assets/fonts/bootstrap', 'public/fonts', true)
+.disableNotifications()
+.webpackConfig({
+    resolve: {
+        alias: {
+            'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+        }
+    }
+});
